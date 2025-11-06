885a43cef282f52f5b8ea7d921e46be102b3b414
unknown <ayla.benyahya20@gmail.com>
2025-11-06 13:23:05 +0100

    dernier commit

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from controllers.plagiat_controller import plagiat_bp
 
 app = Flask(__name__)
-app.config['DEBUG'] = True  # ← Ajoute cette ligne
-app.config['PROPAGATE_EXCEPTIONS'] = True  # ← Et celle-ci
-
 CORS(app)
-
 app.register_blueprint(plagiat_bp)
 
 @app.route('/')
 def home():
-    return jsonify({
-        'message': 'API de détection de plagiat',
-        'status': 'opérationnel'
-    })
+    return render_template('index.html')  # renvoie le fichier HTML
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
